e3950e5f4c9d5003a5bc03ff213d7754a6bcc463
Amy Bertken <amy.bertken@gmail.com>
2015-06-18 20:36:26 -0600

    page displays search result in appropriate size and with html tags for query term

---
+++ public/javascripts/filters.js
@@ -0,0 +1,33 @@
+var app = angular.module('quoteDisplayFilter', [])
+
+
+app.filter('revisedQuotesForDisplay', function() {
+  return function(quote){
+    if (!quote || !quote.length) { return; }
+    var searchTerm = document.getElementById("searchTerm").value;
+    var index;
+    var array = quote.split(" ");
+    var boldedWordInArray = array.map(function(value){
+      if (value.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1){
+        index = array.indexOf(value);
+        return '<span class="super-class">'+ value +'</span>'
+      }else{
+        return value
+        };
+    });
+    if(boldedWordInArray.length >=60){
+      var newQuote = quote;
+      if (index >=30){
+        var spliceAt = index - 30;
+        var newQuoteBegin = boldedWordInArray.splice(spliceAt, 60);
+        newQuoteBegin.unshift("...")
+        newQuote = newQuoteBegin.join(" ") + "...";
+      }else{
+        newQuote = boldedWordInArray.splice(0, 60).join(" ") + "...";
+      }
+      return newQuote;
+    }else{
+    return boldedWordInArray.join(" ");
+    }
+  }
+});
